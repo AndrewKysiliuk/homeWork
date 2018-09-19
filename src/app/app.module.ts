30c6933c18198cd21e21fc36dc7d2b444c26f5b3
andriy <andriy.av95@ukr.net>
2018-09-19 19:26:26 +0300

    home work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {MyModuleModule} from './my-module/my-module.module';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MyModuleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
